d5aafab57bc20eebeddb6ce9324b76496548cab6
minseo <kim-west00@naver.com>
2021-08-14 02:36:29 +0900

    count가 0이냐 1이냐_연속하는 숫자를 셀때 count는 최소 1이여야했다& else문에서의 오류

--- 숫자가 동일한 연속부분 수열.py
+++ 숫자가 동일한 연속부분 수열.py
@@ -1,7 +1,7 @@
 N = int(input())
 before_num = 0
 count = 0
-max_count = 0
+max_count = 1
 for i in range(N):
     #연속하여 동일한 숫자가 나오는 횟수의 최대
     #:횟수가 크면 바로바로 초기화
@@ -11,8 +11,9 @@ for i in range(N):
         if count > max_count:
             max_count = count
     else:
-        max_count = count
-        count = 0
+        if count > max_count:
+            max_count = count
+        count = 1
     before_num = after_num
     
 print(max_count)
