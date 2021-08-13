591516a10a7907b7bed7a99c77c6ec73ee5c82c2
minseo <kim-west00@naver.com>
2021-08-14 04:18:45 +0900

    first commit_indexerror

---
+++ 1이3개이상있는위치.py
@@ -0,0 +1,19 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+#인접한다 = [a][b] 에서 [a-1][b] [a][b-1] [a+1][b] [a][b+1]
+cell_count = 0
+count = 0
+#무식하게 하나하나 다 세볼거다
+for i in range(n):
+    for j in range(n):
+        if arr[i-1][j]==1 and i-1>=0:
+            count +=1
+        elif arr[i][j-1]==1 and j-1>=0:
+            count +=1
+        elif arr[i+1][j] ==1 and i+1 <=n:
+            count +=1
+        elif arr[i][j+1] == 1 and j+1 <=n:
+            count +=1
+        if count >= 3:
+            cell_count +=1
+        count = 0
